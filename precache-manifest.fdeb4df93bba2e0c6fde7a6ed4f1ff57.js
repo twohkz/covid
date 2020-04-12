6ad9b86c9fc2de27abd7d16df421df82886303bd
twohkz <web2bhw@gmail.com>
2020-04-12 21:50:42 +0200

    Updates

---
+++ precache-manifest.fdeb4df93bba2e0c6fde7a6ed4f1ff57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd4c39550787427f4b7b168f8166127",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "03e06066502c62c318f8",
+    "url": "/covid/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4811535235e530b09a9f",
+    "url": "/covid/static/css/main.71eab150.chunk.css"
+  },
+  {
+    "revision": "03e06066502c62c318f8",
+    "url": "/covid/static/js/2.6f429f14.chunk.js"
+  },
+  {
+    "revision": "4c7a7ad3bb98ca993ba3790aed5e9999",
+    "url": "/covid/static/js/2.6f429f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4811535235e530b09a9f",
+    "url": "/covid/static/js/main.23ef085c.chunk.js"
+  },
+  {
+    "revision": "fc9fdcd4ed53cd9133e0",
+    "url": "/covid/static/js/runtime-main.23987d16.js"
+  }
+]);
