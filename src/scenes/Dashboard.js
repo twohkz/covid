22a8ffd43bc6514c37db751a4b74e7ee757a8d14
twohkz <web2bhw@gmail.com>
2020-04-18 22:38:55 +0200

    added a footer

--- src/scenes/Dashboard.js
+++ src/scenes/Dashboard.js
@@ -3,10 +3,30 @@ import React from 'react';
 import Global from './Global';
 import Country from './Country';
 import History from './History';
+import Footer from './Footer';
+
+const pStyle = {
+    fontSize: '150px',
+    Align: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color:'red',
+    
+  };
 
 const Dashboard = () => {
     return (
         <div className='container mt-2'>
+
+            <div className='row'>
+                <div className='col-12 mb-2'>
+                    <h5 className='text-left'>Country Wise Stats</h5>
+                    <hr />
+                </div>
+                <div className='col-12'>
+                    <Country />
+                </div>
+            </div>
             <div className='row mb-3 mt-3'>
                 <div className='col-12'>
                     <h5 className='text-left'>History (Global)</h5>
@@ -25,16 +45,14 @@ const Dashboard = () => {
                     <Global />
                 </div>
             </div>
-            <div className='row'>
-                <div className='col-12 mb-2'>
-                    <h5 className='text-left'>Country Wise Stats</h5>
-                    <hr />
-                </div>
-                <div className='col-12'>
-                    <Country />
-                </div>
+            
+            
+                    <div  className='row mb-3 mt-3' style={pStyle}>
+                        <Footer />
+                    </div>
+            
             </div>
-        </div>
+        
     )
 };
 
---
+++ src/scenes/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                
+                <h6>Created by Hichem Klouz © 2020</h6>
+            </div>
+        )
+    }
+}
